c5c0a934e6d0c1dc626172e4ba7a7e181d012257
TheWillG <wjgarcia2013@gmail.com>
2018-08-10 16:37:49 -0400

    add helmet for security and morgan

--- templates/app/index.ts
+++ templates/app/index.ts
@@ -1,8 +1,13 @@
 import app from "./src/app";
 import mongoose from "mongoose";
-import { logger, port, mongoUrl } from "./lib/config";
+import helmet from "helmet";
+import morgan from "morgan";
+import { logger, port, mongoUrl, isProduction } from "./lib/config";
 
 mongoose.connect(mongoUrl);
+
+app.use(helmet());
+isProduction ? app.use(morgan("combined")) : app.use(morgan("dev"));
 
 app.listen(port);
 logger.info(`App listening on port ${port}!`);
